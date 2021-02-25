c9bb9fe659ba79dc8fd70b675aa331bd4c84b0b9
bincer <vlewis@vip.qq.com>
2021-02-25 17:53:44 +0800

    init website

---
+++ blog/2021-02-25-User-Experience.md
@@ -0,0 +1,9 @@
+---
+slug: User-Experience
+title: Web 用户体验设计提升
+author: Bincer
+description: 所谓的用户体验设计，其实是一个比较虚的概念，是秉承着以用户为中心的思想的一种设计手段，以用户需求为目标而进行的设计。
+tags: [exprience]
+---
+
+## 一、页面展示
---
+++ docusaurus.config.js
@@ -0,0 +1,92 @@
+module.exports = {
+  title: 'Bincer',
+  tagline: 'A single step makes a thousand miles.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'bincer', // Usually your GitHub org/user name.
+  projectName: 'blog', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Bincer',
+      logo: {
+        alt: 'Bincer Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        /* {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        }, */
+        {to: 'blog', label: 'Front-end', position: 'left'},
+        {
+          href: 'https://github.com/bincer/blog',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/bincer/blog',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/bincer/docusaurus/edit/master/website/',
+        },
+        blog: {
+          // showReadingTime: true,
+          // Please change this to your repo.
+          // editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
